356b1468359591a7d8c16f89445ebb10ad47515a
Tom Fish <guerillero.wikipedia@gmail.com>
2020-02-15 01:29:17 -0500

    testing for colin

---
+++ data.js
@@ -0,0 +1,184 @@
+var pts = { "type": "FeatureCollection",
+	"features": [
+		{ "type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [-77.0365, 38.8977]},
+		"properties": {
+			"Name": "Washington, DC, US",
+			"twitter": "",
+      "URL": "https://neoliberalproject.org/washington-dc-neoliberals",
+      "active": true
+			}
+		},
+    { "type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [-74.011265, 40.706877]},
+		"properties": {
+			"Name": "New York, NY, US",
+			"twitter": "https://twitter.com/NYC_Neoliberal",
+      "URL": "https://neoliberalproject.org/nyc-neoliberals-registration",
+      "active": true
+			}
+		},
+    { "type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [-122.4783, 37.8199]},
+		"properties": {
+			"Name": "San Francisco, CA, US <br> (YIMBY Neoliberal)",
+			"twitter": "https://twitter.com/YIMBYNeoliberal",
+      "URL": "https://neoliberalproject.org/san-francisco-neoliberals-registration",
+      "active": true
+			}
+		},
+    { "type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [-118.2437, 34.0522]},
+		"properties": {
+			"Name": "Los Angeles, CA, US",
+			"twitter": "https://twitter.com/NeoliberalLa",
+      "URL": "https://neoliberalproject.org/los-angeles-neoliberals-registration",
+      "active": true
+			}
+		},
+    { "type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [-118.2437, 34.0522]},
+		"properties": {
+			"Name": "Philadelphia, PA, US",
+			"twitter": "https://twitter.com/NeolibPhilly",
+      "URL": "https://neoliberalproject.org/philadelphia-neoliberals-registration",
+      "active": true
+			}
+		},
+    { "type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [-104.9903, 39.7392]},
+		"properties": {
+			"Name": "Denver, CO, US",
+			"twitter": "https://twitter.com/DenverNeoliber1",
+      "URL": "https://neoliberalproject.org/denver-neoliberals-registration",
+      "active": true
+			}
+		},
+    { "type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [-97.7431, 30.2672]},
+		"properties": {
+			"Name": "Austin, TX, US",
+			"twitter": "",
+      "URL": "https://neoliberalproject.org/austin-neoliberals-registration",
+      "active": true
+			}
+		},
+    { "type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [-79.3832, 43.6532]},
+		"properties": {
+			"Name": "Toronto, ON, CAN",
+			"twitter": "",
+      "URL": "https://neoliberalproject.org/toronto-neoliberals-registration",
+      "active": true
+			}
+		},
+    { "type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [-0.1278, 51.5074]},
+		"properties": {
+			"Name": "London, UK",
+			"twitter": "https://twitter.com/LondonNeolibs",
+      "URL": "https://neoliberalproject.org/london-neoliberals-registration",
+      "active": true
+			}
+		},
+    { "type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [-71.0589, 42.3601]},
+		"properties": {
+			"Name": "Boston, MA, US",
+			"twitter": "",
+      "URL": "https://neoliberalproject.org/boston-neoliberals-registration",
+      "active": true
+			}
+		},
+    { "type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [-71.4128, 41.8240]},
+		"properties": {
+			"Name": "Providence, RI, US",
+			"twitter": "",
+      "URL": "https://neoliberalproject.org/providence-neoliberals-registration",
+      "active": true
+			}
+		},
+    { "type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [-86.7816, 36.1627]},
+		"properties": {
+			"Name": "Nashville, TN, US",
+			"twitter": "https://twitter.com/Neolib_Nash",
+      "URL": "https://neoliberalproject.org/nashville-neoliberals-registration",
+      "active": true
+			}
+		},
+    { "type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [-84.3880, 33.7490]},
+		"properties": {
+			"Name": "Atlanta, GA, US",
+			"twitter": "",
+      "URL": "https://neoliberalproject.org/atlanta-neoliberals-registration",
+      "active": true
+			}
+		},
+    { "type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [-81.5494, 28.3747]},
+		"properties": {
+			"Name": "Orlando, FL, US",
+			"twitter": "",
+      "URL": "https://neoliberalproject.org/orlando-neoliberals-registration",
+      "active": true
+			}
+		},
+    { "type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [-87.6170, 41.8663]},
+		"properties": {
+			"Name": "Chicago, IL, US",
+			"twitter": "",
+      "URL": "https://neoliberalproject.org/chicago-neoliberals-registration",
+      "active": true
+			}
+		},
+    { "type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [-122.6750, 45.5051]},
+		"properties": {
+			"Name": "Portland, OR, US",
+			"twitter": "",
+      "URL": "https://neoliberalproject.org/portland-neoliberals-registration",
+      "active": true
+			}
+		},
+    { "type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [-122.3321, 47.6062]},
+		"properties": {
+			"Name": "Seattle, WA, US",
+			"twitter": "",
+      "URL": "https://neoliberalproject.org/seattle-neoliberals-registration",
+      "active": true
+			}
+		},
+    { "type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [-123.1207, 49.2827]},
+		"properties": {
+			"Name": "Vancouver, BC, CAN",
+			"twitter": "",
+      "URL": "https://neoliberalproject.org/vancouver-neoliberals-registration",
+      "active": true
+			}
+		},
+    { "type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [18.0686, 59.3293]},
+		"properties": {
+			"Name": "Stockholm, SWE",
+			"twitter": "https://twitter.com/NeoliberalSe",
+      "URL": "",
+      "active": false
+			}
+		},
+    { "type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [144.9631, -37.8136]},
+		"properties": {
+			"Name": "Melbourne, AUS",
+			"twitter": "",
+      "URL": "https://neoliberalproject.org/melbourne-neoliberals-registration",
+      "active": true
+			}
+		},
+	]
+}
---
+++ map.js
@@ -0,0 +1,32 @@
+function popDefine(twit, uri){
+  if (uri){
+    a = '<br><br><a href="' + uri + '">Meetup Page</a>'
+  }
+  else{
+    a = ""
+  }
+  if (twit){
+    b = '<br><br><a href="' + twit + '">@' + twit.split("/")[3] + '</a>'
+  }
+  else{
+    b = ""
+  }
+  return a+b
+}
+
+
+var chapters = L.geoJSON(pts, {
+  onEachFeature: function(feature, layer){
+    layer.bindPopup(
+      //console.log(popDefine(feature.properties.twitter, feature.properties.URL))
+      '<center><b>' + feature.properties.Name + '</b>' + popDefine(feature.properties.twitter, feature.properties.URL) + '</center>'
+    )
+  }
+});
+
+var map = L.map('MAP').setMaxBounds(chapters.getBounds().pad(0.5)).fitBounds(chapters.getBounds().pad(0.2)).addLayer(chapters);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  noWrap: true
+}).addTo(map);
