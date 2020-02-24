1a7eb6012c7d82c4836225fc9c8deae9c8984b0d
Tom Fish <Guerillero@users.noreply.github.com>
2020-02-24 12:27:00 -0500

    test mapbox

--- map/map.js
+++ map/map.js
@@ -33,7 +33,7 @@ var chapters = L.geoJSON(pts, {
 
 var map = L.map('MAP').setMaxBounds(L.latLngBounds(L.latLng(71.2,179.4), L.latLng(-57.0,-172.0))).fitBounds(chapters.getBounds().pad(0.0)).addLayer(chapters);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://api.mapbox.com/v4/mapbox.satellite/{x}/{y}/{z}@2x.jpg70?access_token=pk.eyJ1IjoiZ3VlcmlsbGVybyIsImEiOiJjazcwcTRvNWUwMTVhM2VuNnd2eW9rMnV4In0.yJdGmpixRrSx-1Ppzz7u8A', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   noWrap: false
 }).addTo(map);
